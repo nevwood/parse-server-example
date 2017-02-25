1cedf79d9a7861efedf4da7068d6e7b1368190cd
nevwood <nev@drysky.co.uk>
2017-02-25 14:08:16 -0800

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,22 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hello!');
 });
 
-Parse.Cloud.define('arrival', function(req, res) {
-  res.success('arrival code');
+Parse.Cloud.define('arrival', function(request, response){
+    Parse.Cloud.useMasterKey();
+    var user = new Parse.User();
+    var query = new Parse.Query("checkin");
+    var phase = "INSITU" 
+    var count;
+    query.include('phase');
+    query.include('username');
+    query.equalTo("phase", phase)
+    query.equalTo("username", request.params.input)
+    query.find().then(function(results) {
+      count = results.length;
+        return Parse.Object.saveAll(results);
+    }).then(function(result) {
+        response.success("success");
+    }, function(error) {
+        response.error(error);
+    });
 });
