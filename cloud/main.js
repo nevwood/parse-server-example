675f02b928eaaad66a10e82c82b7174d035e7350
nevwood <nev@drysky.co.uk>
2017-03-05 17:53:36 +0000

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,22 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hello!');
 });
 
+  Parse.Cloud.define("userArrive", function(request, response) {
+  Parse.Cloud.useMasterKey();
+  // var userRecord = Parse.Object.extend("user");
+  // var userQuery = new Parse.Query(userRecord);   
+    var userQuery = new Parse.Query(Parse.User);  
+  // run query to find current user record
+  userQuery.equalTo("username", request.params.user);
+  userQuery.each(function (userObject) {
+        userObject.set("nrdleMe", "INSITU99");
+        userObject.set("curloc", request.params.checkin);
+        userObject.save({ useMasterKey: true });
+    }).then(function (success) {
+        response.success(99);
+    }, function(err) {
+        response.error(err);
+    });
 
   
   
