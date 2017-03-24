f8ea4f1ac73bb9268b096ab5acef6f99c87e304f
nevwood <nev@drysky.co.uk>
2017-03-25 04:18:56 +1100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -20,6 +20,22 @@ Parse.Cloud.define("userArrive", function(request, response) {
     });
 });
 
+Parse.Cloud.define("userLeave", function(request, response) {
+  Parse.Cloud.useMasterKey();
+  // var record = Parse.Object.extend("checkin");
+  var userQuery = new Parse.Query(Parse.User); 
+  // run query to find specific record
+  userQuery.equalTo("username", request.params.user);
+  userQuery.each(function (object) {
+        object.set("nrdleMe", "MOVE");
+        object.set("curloc", request.params.checkin);
+        object.save({ useMasterKey: true });
+    }).then(function (success) {
+        response.success(99);
+    }, function(err) {
+        response.error(err);
+    });
+});
   
   
   
