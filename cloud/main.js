da130baca5751aa93f190255ddd8cb431bbf1661
nevwood <nev@drysky.co.uk>
2017-05-01 13:25:28 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -40,17 +40,15 @@ Parse.Cloud.define("userLeave", function(request, response) {
 
 Parse.Cloud.define('updateNearbyUser', function(request, response) {
     Parse.Cloud.useMasterKey();
-    var userId = request.params.userId;
-    var uniqueNuurdleId = request.params.uniqueNuurdleId;
-
     var query = new Parse.Query(Parse.User);
-    query.get(userId).then(function(user) {
-        user.addUnique("nearbyNuurdles", uniqueNuurdleId);
-        return user.save(null, {useMasterKey:true});
-    }).then(function(user) {
-        response.success(user);
-    }, function(error) {
-        response.error(error);
+    query.equalTo("username", request.params.userId);
+    query.each(function (object) {
+      object.addUnique("nearbyNuurdles", request.params.uniqueNuurdleId);
+      object.save({ useMasterKey: true });
+    }).then(function (success) {
+        response.success(99);
+    }, function(err) {
+        response.error(err);
     });
 });
   
