3a2fe09b38dc2769ad40af43f3641316b7609554
nevwood <nev@drysky.co.uk>
2017-05-29 21:54:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ var url = require('url');
 
 var parse_api = new ParseServer({
   databaseURI: process.env.MONGOLAB_URI || 'mongodb://nuurdleclonedb-parse:eHwb9tDVUUk3tDCT9QVIDSXAzvW3oH3MmdnufxU18X9Y2F5blWtzQE3x50L7f8IWd9Uprhg99PxDuyZ0eeSqrA==@nuurdleclonedb-parse.documents.azure.com:10250/parse?ssl=true',
-  // cloud: './cloud/main.js',
-  // appId: '614922c7-3ff4-4401-bb21-e4ac67fdd2cd',
+    cloud: './cloud/main.js',
+    appId: '614922c7-3ff4-4401-bb21-e4ac67fdd2cd',
   // fileKey: 'xxxxxxx',
-  // masterKey: '6b68e534-bb1f-4785-972a-7682dc2f7016',
-  // serverURL: process.env.PARSE_SERVER_URL || 'https://nuurdleprod.azurewebsites.net/parse'
+    masterKey: '6b68e534-bb1f-4785-972a-7682dc2f7016',
+    serverURL: process.env.PARSE_SERVER_URL || 'https://nuurdleprod.azurewebsites.net/parse'
 });
 
 var config = parseServerConfig(__dirname);
